c7640cc498a5cfacc10035be4e9fce2fdd6af9ed
Gururaj Krishna <gururajkrishna106@gmail.com>
2025-07-24 16:56:41 -0700

    Added Screen Two text

---
+++ SourceControlApp/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SourceControlApp
+//
+//  Created by Gururaj Krishna on 7/24/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen Two!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
