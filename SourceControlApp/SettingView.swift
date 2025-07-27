5355f4615296cf73e087d4bdaf3e8699e0ffa29c
Gururaj Krishna <gururajkrishna106@gmail.com>
2025-07-27 16:16:35 -0700

    added SettingView

---
+++ SourceControlApp/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  SourceControlApp
+//
+//  Created by Gururaj Krishna on 7/27/25.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
