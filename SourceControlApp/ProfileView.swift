aa3f88585f73327fdb694a3214ca1f92c204c6d5
Raichur456 <gururajkrishna106@gmail.com>
2025-07-27 21:19:58 -0700

    Feature/add profile view (#5)
    * Added Profile View and created 3 Heart Emojis
    
    * added a state  boolean variable called isChanged

---
+++ SourceControlApp/ProfileView.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileView.swift
+//  SourceControlApp
+//
+//  Created by Gururaj Krishna on 7/27/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    @State var isChanged: Bool = true
+    var body: some View {
+            ForEach(0..<3, id: \.self) { index in
+                HStack {
+                    Text("Heart Emoji")
+                        
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(.red)
+                }
+                .font(.largeTitle)
+                .padding()
+            }
+                Spacer()
+    }
+}
+
+#Preview {
+    ProfileView()
+}
