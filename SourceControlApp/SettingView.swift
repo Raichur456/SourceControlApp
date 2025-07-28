481204e2b7809f1685deac71a20901b51e5a183a
Raichur456 <gururajkrishna106@gmail.com>
2025-07-27 18:42:51 -0700

    [Update] updated settingView (#4)

--- SourceControlApp/SettingView.swift
+++ SourceControlApp/SettingView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct SettingView: View {
-    @State var rise = false
+    @State var rise = true
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
         Text("Added Notification")
